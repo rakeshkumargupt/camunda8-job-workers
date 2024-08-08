6e3e32bb4bcaf2b8243bcae6f4c84bb4488bffe9
Rakesh Gupta <consultant-rakeshkumar.gupta@mcb.mu>
2024-08-08 17:33:22 +0530

    Removed scheduler and used auto poll

--- camunda8-java-jobworker/src/main/java/com/rkg/learning/camunda8/worker/CompletePaymentWorker.java
+++ camunda8-java-jobworker/src/main/java/com/rkg/learning/camunda8/worker/CompletePaymentWorker.java
@@ -2,8 +2,8 @@ package com.rkg.learning.camunda8.worker;
 
 import com.rkg.learning.camunda8.handler.CreditCardServiceHandler;
 import io.camunda.zeebe.client.ZeebeClient;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.time.Duration;
@@ -15,7 +15,7 @@ public class CompletePaymentWorker {
     private final ZeebeClient zeebeClient;
     private final CreditCardServiceHandler creditCardServiceHandler;
 
-    @Scheduled(fixedRate = 60000)
+    @PostConstruct
     public void checkPaymentJob() throws InterruptedException {
         System.out.println("Connected to: " + zeebeClient.newTopologyRequest().send().join());
         zeebeClient.newWorker()
